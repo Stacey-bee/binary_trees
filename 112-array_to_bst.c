14e02c0a545c2fa663d92512c72a0c82d22b45b7
KIGARDE TOM <75412361+MISTOM@users.noreply.github.com>
2021-12-03 06:13:53 +0300

    Update 112-array_to_bst.c
    Add description | Refactor code | i of type size_t

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -2,22 +2,21 @@
 
 /**
  * array_to_bst - builds a Binary Search Tree from an array
- * @array: pointer to the first element in the array
- * @size: number of elements in the array
- * Return: pointer to the root node of the created bst or null
+ * @array: pointer to the first element of the array to be converted
+ * @size: number of element in the array
+ *
+ * Description: If a value of the array already exists, this value is ignored
+ *
+ * Return: pointer to the root node of the created BST, or NULL on failure
  */
-
 bst_t *array_to_bst(int *array, size_t size)
 {
-	int i;
 	bst_t *root = NULL;
+	size_t i;
 
-	if (array != NULL)
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < (int)size; i++)
-		{
-			bst_insert(&root, *(array + i));
-		}
+		bst_insert(&root, array[i]);
 	}
 	return (root);
 }
